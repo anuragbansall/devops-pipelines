ab3fb6adc51f8de8a37f818e629d2553057b0428
Anurag Bansal <devanuragbansal@gmail.com>
2025-12-25 16:40:06 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Hello, DevOps!");
+console.log(
+  "This is a sample Node.js application to learn DevOps CI/CD pipelines."
+);
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting tests...");
+console.log("Waiting 3 seconds...");
+setTimeout(() => {
+  console.log("Tests completed.");
+}, 3000);
